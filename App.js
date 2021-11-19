2598affe75ede3afcfe0901585645f4ef0e7825d
neka1519 <71866083+neka1519@users.noreply.github.com>
2021-11-19 12:38:06 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from'react' ;
+import{View, Text,Button,StyleSheet} from'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App =()=> {
+
+const[name,setName] = useState('Neka');
+//the initial value will be set to Neka
+const[session, setSession] = useState({number :1,
+   title:'Hoos and States'})
+const[current, setCurrent] = useState(true)
+
+const updateHandler =()=>{
+  setName('Your name is Neka')
+  setSession({number : 2})
+  setCurrent (false)
 }
 
+
+  return(
+<View style = {styles.container}>
+  <Text  styles = {styles.textContainer}>{name} </Text>
+  <Text  styles = {styles.textContainer}> 
+  You are in the session {session.number}
+   </Text>
+   <Text  styles = {styles.textContainer}> 
+  {current ? 'You are in current Session' : 'You have moved to next session' }
+   </Text>
+    <Button title = "Update" 
+    color ='#000000'
+    onPress = {updateHandler}>
+    </Button>
+</View>
+  )
+  }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    backgroundColor : '#678dff',
+    alignContent : 'center',
+    alignItems : 'center'
   },
-});
+
+
+  textContainer :  {
+    fontSize : 25,
+    fontWeight : 'bold',
+    color : '#ff9023',
+    marginTop : 100
+  }
+})
+
+
+
+
+
+export default App;
